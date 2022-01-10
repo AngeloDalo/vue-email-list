ede9e0e302fc00cba6ac3b169ef167f435d00077
AngeloDalo <angelo251158@gmail.com>
2022-01-10 15:31:06 +0100

    loop

--- script.js
+++ script.js
@@ -9,21 +9,26 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-           email: null,
+           emails: [],
         },
 
         created() {
-            axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((response) => {
-              this.email = response.data.response;
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+            console.log(this.emails);
         },
 
         methods: { 
-
+            generateEmail(index) {
+                for (index=0; index<10; index++) {
+                    axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                      console.log(response.data.response);
+                      this.emails.push(response.data.response);
+                    })
+                    .catch(function (error) {
+                      console.log(error);
+                    });
+                }
+            }
         }
     });
