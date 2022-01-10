02ec686c87242bc81f5866529eba17d3e5d67dc0
AngeloDalo <angelo251158@gmail.com>
2022-01-10 15:56:32 +0100

    no_loop

--- script.js
+++ script.js
@@ -13,22 +13,19 @@ const app = new Vue(
         },
 
         created() {
-            console.log(this.emails);
+            for (let i=0; i<10; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            }
         },
 
         methods: { 
-            generateEmail() {
-                for (let i=0; i<10; i++) {
-                    axios
-                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-                    .then((response) => {
-                      console.log(response.data.response);
-                      this.emails.push(response.data.response);
-                    })
-                    .catch(function (error) {
-                      console.log(error);
-                    });
-                }
-            }
+
         }
     });
